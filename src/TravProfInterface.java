9c29e42e043da7da4c0aed357d80768e71d5bd4e
Kolbin, Alexander <AKolbin24@massmutual.com>
2020-10-26 14:02:35 -0400

    implemented some methods

--- src/TravProfInterface.java
+++ src/TravProfInterface.java
@@ -1,26 +1,42 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TravProfInterface {
+    ArrayList<TravProf> travProfs;
+    String dbName = "TravProfDB";
+    public TravProfInterface(String fileName){
+        travProfs = new ArrayList<TravProf>();
+    }
+
+    public boolean getUserChoice(){
+        System.out.println("(1) Enter a New TravProf");
+        System.out.println("(2) Delete a traveler by Name");
+        System.out.println("(3) Exit");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        int int_input = Integer.parseInt(input); //Implement error handling for non-integer inputs.
+
+        if(int_input == 1){
+            System.out.println("call createNewTravProf here");
+            return true;
+        }
+        if(int_input == 2){
+            System.out.println("call deleteTravProf here");
+            return true;
+        }
+        if(int_input == 3){
+            System.out.println("Exiting");
+            return false;
+        }
+        System.out.println("The option you selected was invalid, please choose from the following list:");
+        return true;
+    }
+
     public static void main(String[] args){
         boolean state = true;
+        TravProfInterface db = new TravProfInterface("test");
         while(state){
-            System.out.println("(1) Enter a New TravProf");
-            System.out.println("(2) Delete a traveler by Name");
-            System.out.println("(3) Exit");
-            Scanner scan = new Scanner(System.in);
-            String input = scan.nextLine();
-            int int_input = Integer.parseInt(input);
-
-            if(int_input == 1){
-                System.out.println("call createNewTravProf here");
-            }
-            if(int_input == 2){
-                System.out.println("call deleteTravProf here");
-            }
-            if(int_input == 3){
-                System.out.println("Exiting");
-                state = false;
-            }
+            state = db.getUserChoice();
         }
     }
 }
