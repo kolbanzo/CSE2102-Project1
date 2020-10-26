b9e673c2384dd42dccba7f040cbf4475121d4259
Kolbin, Alexander <AKolbin24@massmutual.com>
2020-10-26 17:44:21 -0400

    implemented displayTravProf, deleteTravProf, findTravProf, updateTravProf methods and added comments for questions to ask TA

--- src/TravProfInterface.java
+++ src/TravProfInterface.java
@@ -1,11 +1,24 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class TravProfInterface {
-    ArrayList<TravProf> travProfs;
-    String dbName = "TravProfDB";
+public class TravProfInterface { //Should we handle multiple profiles with the same last name and the same trav agent ID?
+    ArrayList<TravProf> travProfs; //Ex: for findTravProf(), which profile should we display for family
+    String dbName;
     public TravProfInterface(String fileName){
         travProfs = new ArrayList<TravProf>();
+        dbName = fileName;
+    }
+
+    void displayTravProf(TravProf travProf){
+        System.out.println("Travel Agent ID: " + travProf.gettravAgentID());
+        System.out.println("Traveler's First Name: " + travProf.getFirstName());
+        System.out.println("Traveler's Last Name: " + travProf.getLastName());
+        System.out.println("Traveler's Address: " + travProf.getAddress());
+        System.out.println("Traveler's Phone Number: " + travProf.getPhone());
+        System.out.println("Cost of Trip: " + travProf.getTripCost());
+        System.out.println("Travel Type: " + travProf.getTravelType());
+        System.out.println("Payment Type: " + travProf.getPaymentType());
+        System.out.println();
     }
 
     void createNewTravProf(){
@@ -39,24 +52,89 @@ public class TravProfInterface {
         travProfs.add(newTravProf);
     }
 
+    void deleteTravProf(){ //What if no profile on record with provided last name?
+        Scanner deleteScanner = new Scanner(System.in);
+
+        System.out.println("Enter last name of profile");
+        String lastName = deleteScanner.nextLine();
+
+        System.out.println("Enter Travel Agent ID:");
+        String travAgentID = deleteScanner.nextLine();
+
+        for(int i = 0; i < travProfs.size(); i++){
+            TravProf tempProf = travProfs.get(i);
+            if(tempProf.getLastName().equals(lastName) && tempProf.gettravAgentID().equals(travAgentID)){
+                travProfs.remove(tempProf);
+            }
+        }
+
+    }
+
+    void findTravProf(){
+        Scanner findScanner = new Scanner(System.in);
+
+        System.out.println("Enter last name of profile");
+        String lastName = findScanner.nextLine();
+
+        System.out.println("Enter Travel Agent ID:");
+        String travAgentID = findScanner.nextLine();
+
+        for(int i = 0; i < travProfs.size(); i++){
+            TravProf tempProf = travProfs.get(i);
+            if(tempProf.getLastName().equals(lastName) && tempProf.gettravAgentID().equals(travAgentID)){
+                displayTravProf(tempProf);
+            }
+        }
+    }
+
+    void updateTravProf(){ //How do we determine which profile they want to modify? By last name?
+        Scanner updateScanner = new Scanner(System.in);
+        TravProf modifyProf = new TravProf(null, null, null, null, null,
+                0, null, null);
+
+        System.out.println("Enter last name of profile");
+        String lastName = updateScanner.nextLine();
+
+        for(int i = 0; i < travProfs.size(); i++){
+            TravProf tempProf = travProfs.get(i);
+            if(tempProf.getLastName().equals(lastName)){
+                modifyProf = tempProf;
+            }
+        }
+
+        System.out.println("Select which attribute you would like to modify: "); //Menu with number selection?
+        System.out.println("(1) Address");
+        System.out.println("(2) Phone");
+        System.out.println("(3) Travel Type");
+        System.out.println("(4) Trip Cost");
+        System.out.println("(5) Payment Type");
+        System.out.println("(6) MD Contact");
+        System.out.println("(7) MD Phone");
+        System.out.println("(8) Illness Type");
+        System.out.println("(9) Allergy Type");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        int int_input = Integer.parseInt(input); //Implement error handling for non-integer inputs?
+
+        System.out.println("Enter updated value");
+        String updatedValue = updateScanner.nextLine();
+
+        if(int_input == 1){
+            modifyProf.updateAddress(updatedValue);
+        }
+    }
+
     void displayAllTravelProfiles(){
         for(int i = 0; i < travProfs.size(); i++){
             TravProf tempProf = travProfs.get(i);
-            System.out.println("Travel Agent ID: " + tempProf.travAgentID);
-            System.out.println("Traveler's First Name: " + tempProf.firstName);
-            System.out.println("Traveler's Last Name: " + tempProf.lastName);
-            System.out.println("Traveler's Address: " + tempProf.address);
-            System.out.println("Traveler's Phone Number: " + tempProf.phone);
-            System.out.println("Cost of Trip: " + tempProf.tripCost);
-            System.out.println("Travel Type: " + tempProf.travelType);
-            System.out.println("Payment Type: " + tempProf.paymentType);
+            displayTravProf(tempProf);
         }
     }
 
     public boolean getUserChoice(){
-        System.out.println("(1) Enter a New TravProf");
-        System.out.println("(2) Delete a traveler by Name");
-        System.out.println("(3) Find a traveler");
+        System.out.println("(1) Create a new travel profile");
+        System.out.println("(2) Delete a travel profile");
+        System.out.println("(3) Find a travel profile");
         System.out.println("(4) Modify a travel profile");
         System.out.println("(5) Display all travel profiles");
         System.out.println("(6) Write to database");
@@ -64,35 +142,35 @@ public class TravProfInterface {
         System.out.println("(8) Exit");
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
-        int int_input = Integer.parseInt(input); //Implement error handling for non-integer inputs.
+        int int_input = Integer.parseInt(input); //Implement error handling for non-integer inputs?
 
         if(int_input == 1){
             createNewTravProf();
             return true;
         }
         if(int_input == 2){
-            System.out.println("call deleteTravProf here");
+            deleteTravProf();
             return true;
         }
         if(int_input == 3){
-            System.out.println("call findTravProf");
-            return false;
+            findTravProf();
+            return true;
         }
         if(int_input == 4){
-            System.out.println("call updateTravProf");
-            return false;
+            updateTravProf();
+            return true;
         }
         if(int_input == 5){
             displayAllTravelProfiles();
-            return false;
+            return true;
         }
         if(int_input == 6){
             System.out.println("call writeToDB");
-            return false;
+            return true;
         }
         if(int_input == 7){
             System.out.println("call initDB");
-            return false;
+            return true;
         }
         if(int_input == 8){
             System.out.println("Exiting");
