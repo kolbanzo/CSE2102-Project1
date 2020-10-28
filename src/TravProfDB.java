86ea8ce8f93be88d32129fb5b642c99d71eedb6f
jackreever <jack.reever@uconn.edu>
2020-10-28 11:25:19 -0400

    fixed travprofDB to be arraylist structure

--- src/TravProfDB.java
+++ src/TravProfDB.java
@@ -1,40 +1,28 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TravProfDB {
 
     private int numTravelers; //how many travelers are in DB
     private int openTravelerIndex; //gives position in travelerList of next open spot
     private String fileName; //name of DB
-    private TravProf[] travelerList; //array of all profiles in DB
+    private ArrayList travelerList; //array of all profiles in DB
     private int currentTravelerIndex; //used as an iterator
 
     //constructor
     public TravProfDB(String fileName) {
         this.fileName = fileName;
         this.numTravelers = 0;
-        this.travelerList = new TravProf[10];
+        this.travelerList = new ArrayList();
         this.openTravelerIndex = 0;
         this.currentTravelerIndex = 0;
     }
 
     //Insert new profile accepts a TravProf as input and inserts it into the travelerList
     private void insertNewProfile(TravProf profile) {
-        //make sure the array can fit the new profile
-        if (this.travelerList.length <= this.openTravelerIndex) {
-            //if we need more space, double the length of the list
-            TravProf[] newTravelerList = new TravProf[this.numTravelers * 2];
-            //copy old data into bigger list
-            for (int i = 0; i < this.travelerList.length; i++) {
-                newTravelerList[i] = this.travelerList[i];
-            }
-            this.travelerList = newTravelerList;
-        }
-        //now that we have space, insert new profile, and add to the number of travelers
-        //also update the index which points to the next "open" spot
-        this.travelerList[this.openTravelerIndex] = profile;
-        this.openTravelerIndex++;
+            this.travelerList.add(profile);
         this.numTravelers++;
     }
 
@@ -42,41 +30,38 @@ public class TravProfDB {
     private boolean deleteProfile(String travID, String lastName) {
         boolean successfulDeletion = false;
         //find a profile with provided information
-        for (int i = 0; i < this.openTravelerIndex; i++) {
-            TravProf deleteCandidate = this.travelerList[i];
+        for (int i = 0; i < this.travelerList.size(); i++) {
+            TravProf deleteCandidate = (TravProf) this.travelerList.get(i);
             if (deleteCandidate.gettravAgentID().equals(travID) && deleteCandidate.getLastName().equals(lastName)) {
-                for (int j = i; j < this.openTravelerIndex - 1; j++) {
-                    travelerList[j] = travelerList[j + 1];
-                }
+                this.travelerList.remove(i);
                 successfulDeletion = true;
+                }
             }
-        }
-        if(successfulDeletion){this.openTravelerIndex--;}
         return successfulDeletion;
     }
 
     //findProfile will search for an ID and last name and return a profile
     private TravProf findProfile(String travID, String lastName){
+        int j = 0;
         for(int i=0; i<this.openTravelerIndex;i++){
-            TravProf searchCandidate  = this.travelerList[i];
+            TravProf searchCandidate  = (TravProf) this.travelerList.get(i);
             if(searchCandidate.gettravAgentID().equals(travID) && searchCandidate.getLastName().equals(lastName)){
-                return searchCandidate;
-                //int j = i
+                j=i;
+                break;
             }
         }
-        //this says missing return statement, but a travProf should be returned if it exists in the list?
-        //return this.travelerList[j]
+        return (TravProf) this.travelerList.get(j);
     }
 
     //findFirstProfile
     private TravProf findFirstProfile(){
         //this.currentTravelerIndex = 0;
-        return this.travelerList[0];
+        return (TravProf) this.travelerList.get(0);
     }
 
     //findNextProfile
     private TravProf findNextProfile(){
-        TravProf nextProf = this.travelerList[this.currentTravelerIndex];
+        TravProf nextProf = (TravProf) this.travelerList.get(this.currentTravelerIndex);
         currentTravelerIndex++;
         return nextProf;
     }
@@ -86,7 +71,7 @@ public class TravProfDB {
         //in the travelerList
         FileWriter dbFile = new FileWriter(fileName);
         for (int i = 0; i<this.openTravelerIndex; i++){
-            TravProf currentTrav = this.travelerList[i];
+            TravProf currentTrav = (TravProf) this.travelerList.get(i);
             dbFile.write("traveler ID: "+ currentTrav.gettravAgentID());
             dbFile.write("traveler Name: "+ currentTrav.getFirstName()+currentTrav.getLastName());
             dbFile.write("traveler Address: "+ currentTrav.getAddress());
