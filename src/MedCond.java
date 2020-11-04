2d1ca2d11fe9d9e1fc6bea5bd20e126d84889067
jackreever <jack.reever@uconn.edu>
2020-11-04 13:29:37 -0500

    adding a little documentation to medcond

--- src/MedCond.java
+++ src/MedCond.java
@@ -3,13 +3,16 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class MedCond implements Serializable {
+    //declare all attributes
     public String mdContact, mdPhone, algType, illType;
+    //constructor
     public MedCond(String contact, String phone, String alg, String ill){
         this.mdContact = contact;
         this.mdPhone = phone;
         this.algType = alg;
         this.illType = ill;
     }
+    //all functions are simple updates or attribute retrievals
     public String getMdContact(){return mdContact;}
     public String getMdPhone(){return mdPhone;}
     public String getAlgType(){return algType;}
